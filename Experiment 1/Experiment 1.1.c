2551517af21bfe9a16ad6bd2e7d8407f9d032329
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-08-27 21:59:38 +0530

    Create Experiment 1.1.c

---
+++ Experiment 1/Experiment 1.1.c
@@ -0,0 +1,12 @@
+  /*Experiment1: Installation, Environment Setup and starting with c language
+ 1.Write a C program to print "Hello World"*/
+ 
+#include<stdio.h>
+main()
+{
+	printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+	printf("Hello World");//this statement is for printing
+	return 0;  
+}
+
+ 
