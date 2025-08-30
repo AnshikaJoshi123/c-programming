8d1fe7a2274786fbb9414ac18b8a34da093ddc26
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-08-30 22:51:04 +0530

    Create Experiment 1.5.c

---
+++ Experiment 1/Experiment 1.5.c
@@ -0,0 +1,15 @@
+/*Experiment2:Operator
+4.WAP a C program to calculate the area and perimeter of a rectangle based on its length and width*/
+#include <stdio.h>
+ int main()
+ {
+ 	int l,b,p;// declaration;
+         printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+ 	printf("Enter the value of l");
+ 	scanf("%d",&l);
+ 	printf("Enter the value of b");
+ 	scanf("%d",&b);
+ 	p=2*(l+b);
+ 	printf("the perimeter of rectangle is =%d\n",p);
+ 	return 0;
+ }
