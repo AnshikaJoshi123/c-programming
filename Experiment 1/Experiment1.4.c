2ccca180054c4da969feab31a8aa564a61a94f10
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-08-31 18:56:27 +0530

    Update Experiment1.4.c

--- Experiment 1/Experiment1.4.c
+++ Experiment 1/Experiment1.4.c
@@ -4,8 +4,9 @@
  int main()
  {
  	int a,b;// declaration
- 	int s,c,k,j;
-  printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+ 	int s,c,k;
+	float j;
+    printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
  	printf("Enter the value of a:");
  	scanf("%d",&a);
  	printf("Enter the value of b:");
@@ -16,7 +17,7 @@
  	printf("the subtraction of a-b is=%d\n",c);
  	k=a*b;
  	printf("the multiplication of a*b is=%d\n",k);
- 	j=a/b;
- 	printf("the division of a/b is=%d\n",j);
+ 	j=(float)a/b;
+ 	printf("the division of a/b is=%f\n",j);
  	return 0;	
  }
