aa9178e0e66fc36dd072ac0f9637bb64fe662d8b
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-09-19 14:28:25 +0530

    Create Experiment3.1.c

---
+++ Experiment3/Experiment3.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int a;
+	printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+	printf("Enter the values of a");
+	scanf("%d",&a);
+	
+	{
+		if (a%2==0)
+		printf("the number a is even");
+		
+		else 
+		printf("the number a is odd");
+	}
+	
+	return 0;
+}
