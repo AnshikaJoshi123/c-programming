361a8a012102e0749fda4f39e678ee8cf0fd8fd9
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-09-19 15:09:47 +0530

    Update Experiment3.2.c

--- Experiment3/Experiment3.2.c
+++ Experiment3/Experiment3.2.c
@@ -1,48 +1,54 @@
 #include <stdio.h>
 #include <math.h>
+
 int main(void) {
-    int a,b,c, sum=0;
-    printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
-    printf("Enter the values for a,b,c");
-    scanf("%d%d%d",&a,&b,&c);
-    int h,perpendicular,base;
-    if((a>b)&&(a>c))
-    {
-        h =a;
+    int a, b, c;
+    printf("Name - Anshika Joshi\nSap id - 590028351\nBatch - B1\nCourse - Bsc-CS\n");
+    printf("Enter the values for a, b, c: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    int h, perpendicular, base;
+
+
+    if ((a > b) && (a > c)) {
+        h = a;
         perpendicular = b;
         base = c;
     }
-     else if((b>a)&&(b>c))
-    {
-        h =b;
+    else if ((b > a) && (b > c)) {
+        h = b;
         perpendicular = a;
         base = c;
     }
-    else
-    {
-        h =c;
+    else {
+        h = c;
         perpendicular = a;
         base = b;
     }
-    if(((a+b)>c)||((b+c)>a)||((a+c)>b))
-    {
-         if((a==b)&&(b==c))
-        {
-            printf("Equilateral");
+
+   
+    if (((a + b) > c) && ((b + c) > a) && ((a + c) > b)) {
+        printf("Valid Triangle\n");
+
+       
+        if ((a == b) && (b == c)) {
+            printf("Equilateral Triangle\n");
         }
-        else if((a==b)||(b==c)||a==c)
-        {
-            printf("Isosceles");
+        else if ((a == b) || (b == c) || (a == c)) {
+            printf("Isosceles Triangle\n");
         }
-        else if((h*h)==(perpendicular*perpendicular)+(base*base))
-        {
-             printf("Right Angled traingle");
-        }
-        else
-        {
-            printf("Scalene");
+        else {
+            printf("Scalene Triangle\n");
         }
 
+
+        if ((h * h) == (perpendicular * perpendicular) + (base * base)) {
+            printf("Right-angled Triangle\n");
+        }
     }
+    else {
+        printf("Not a valid triangle\n");
+    }
+
     return 0;
 }
