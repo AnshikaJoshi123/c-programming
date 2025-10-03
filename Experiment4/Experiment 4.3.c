630479e5f926dedefe7efe3a95f124ef85597495
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-10-03 21:06:29 +0530

    Update Experiment 4.3.c

--- Experiment4/Experiment 4.3.c
+++ Experiment4/Experiment 4.3.c
@@ -1,3 +1,5 @@
+/*Experiment 3: Conditional Statements
+3. WAP using ternary operator, the user should input the length and breadth of a rectangle, one has to find out which rectangle has the highest perimeter. The minimum number of rectangles should be three.*/
 #include <stdio.h>
 int main()
  {
@@ -29,5 +31,3 @@ int main()
 
     return 0;
 }
-
-
