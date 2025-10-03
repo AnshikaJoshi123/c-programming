54afaf80eac54e2307ac40f83d491e1d0afc3815
AnshikaJoshi123 <anshikajoshi1726@gmail.com>
2025-10-03 19:25:34 +0530

    Create Experiment 4.1.c

---
+++ Experiment4/Experiment 4.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+ {
+    int x1, y1, x2, y2, x3, y3;
+    int area;
+
+    printf("Name-Anshika joshi\nSap id-590028351\nBatch-B1\nCourse-Bsc-cs\n");
+    printf("Enter coordinates of point1(x1 y1):");
+    
+    scanf("%d %d", &x1, &y1);
+    printf("Enter coordinates of point2(x2 y2):");
+    scanf("%d %d", &x2, &y2);
+
+    printf("Enter coordinates of point3(x3 y3) ");
+    scanf("%d %d", &x3, &y3);
+
+    area = x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2);
+
+    if (area == 0)
+        printf("The points are collinear\n");
+    else
+        printf("The points are not collinear.\n");
+
+    return 0;
+}
